97868439c28e0551149ef43fa13fcacd45d5ab60
linus <84990808+linuso71@users.noreply.github.com>
2024-05-01 20:48:36 +0000

    data types , conversion and comparison added

---
+++ 03_conversionOperation.js
@@ -0,0 +1,27 @@
+let isLogged = ""
+let booleanIsLogged = Boolean(isLogged)
+console.log(booleanIsLogged);
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let score = "33"
+let numberScore = Number(score)
+console.log(typeof numberScore);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1,2,3....; false => 0
+
+
+
+
+
+
+
+// ********************************* Operations ***********************************************
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 3)
+console.log(1 + 2 + "3")
---
+++ 04_comparision.js
@@ -0,0 +1,12 @@
+// console.log("1">1);
+// console.log("012">1);
+// console.log("012abc">1);
+
+console.log(null > -1);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+// == convert diffrently than > , >= etc operators
+
+console.log("2" === 2); // check data type also in comparison
